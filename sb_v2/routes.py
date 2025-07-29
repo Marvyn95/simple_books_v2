9e47591ab176e753b2dc8765fd8db3db6dcb7b66
Marvyn95 <marvinluyombya@gmail.com>
2025-07-29 23:46:46 +0300

    minor

--- sb_v2/routes.py
+++ sb_v2/routes.py
@@ -1,6 +1,21 @@
 from sb_v2 import app
-from flask import render_template
+from flask import render_template, request
+import json
+
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route("/register_owner", methods=["GET", "POST"])
+def register_owner():
+    with open("../config.json") as config_file:
+        config = json.load(config_file)
+
+    if request.method == "POST":
+        form_info = request.form
+        if form_info["admin_password"] == config["ADMIN_PASSWORD"]:
+            
+            return render_template("register_owner_success.html")
+    else:
+        return render_template("register_owner.html")
